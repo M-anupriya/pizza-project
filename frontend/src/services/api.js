e95c627c35ae7b279506dc9d336760ba572285bc
M-anupriya <anupriyamurugan111@gmail.com>
2026-06-02 08:05:32 +0530

    fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pizza-project-1-xosi.onrender.com/api",
 });
 
 export default API;
